8565fa2566e2186692955a46d5a80bedd7aa613f
Paweł Benkowski <pawlo97.pb@gmail.com>
2019-11-22 20:25:48 +0100

    Requested changes made, eslint-plugin-prettier and prettier were missing in package.json - fixed that, prettierrc matches new rules

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div className="app">
-        <h1>Setup Done</h1>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <div className="app">
+                <h1>Setup Done</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
