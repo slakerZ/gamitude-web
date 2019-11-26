5a109bfc8577614f7d515771d56c03b19d15213b
Paweł Benkowski <pawlo97.pb@gmail.com>
2019-11-26 20:15:22 +0100

    Layouts for desktop, tablet and mobile complete

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -1,25 +1,11 @@
 import React from "react";
-import { Picture } from "react-responsive-picture";
 // Styles
 import "./home.styles.scss";
 const HomePage = () => {
     return (
         <div className="homepage">
-            <div className="rank">
-                <Picture
-                    className="picture"
-                    sources={[
-                        {
-                            srcSet: require("../../assets/ranks_default/0-0-0-0-tablet.jpg"),
-                            media: "(min-width: 520px)",
-                        },
-                        {
-                            srcSet: require("../../assets/ranks_default/0-0-0-0-desktop.jpg"),
-                            media: "(min-width: 1080)",
-                        },
-                    ]}
-                />
-            </div>
+            <nav className="nav">nav</nav>
+            <div className="rank">rank</div>
             <div className="projects">projects</div>
             <div className="energies">energies</div>
             <div className="stats">stats</div>
