43114f63202cd1e6163431f92dde2fa434460b20
Paweł Benkowski <pawlo97.pb@gmail.com>
2019-11-29 18:30:03 +0100

    Progressing transition to material-ui based navigation

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Paper, Tabs, Tab } from "@material-ui/core";
 //Styles
 import "./navigation.styles.scss";
 // SVG's
@@ -8,26 +9,54 @@ import { ReactComponent as BulletJournalIcon } from "../../assets/icons/journal.
 import { ReactComponent as Logo } from "../../assets/icons/sloth.svg";
 
 const Navigation = () => {
+    // return (
+    //     <nav className="navigation">
+    //
+    //             <div className="nav-item">
+    //                 <Logo className="icon" />
+    //             </div>
+    //         </Link>
+    //
+    //             <div className="nav-item">
+    //                 <ProjectsIcon className="icon" />
+    //                 Projects
+    //             </div>
+    //         </Link>
+    //
+    //             <div className="nav-item">
+    //                 <BulletJournalIcon className="icon" />
+    //                 Bullet Journal
+    //             </div>
+    //         </Link>
+    //     </nav>
+    // );
+    const iconStyles = { width: "6vh", height: "6vh" };
     return (
-        <nav className="navigation">
-            <Link to="/" className="nav-link">
-                <div className="nav-item">
-                    <Logo className="icon" />
-                </div>
-            </Link>
-            <Link to="/projects" className="nav-link">
-                <div className="nav-item">
-                    <ProjectsIcon className="icon" />
-                    Projects
-                </div>
-            </Link>
-            <Link to="/bulletJournal" className="nav-link">
-                <div className="nav-item">
-                    <BulletJournalIcon className="icon" />
-                    Bullet Journal
-                </div>
-            </Link>
-        </nav>
+        <Paper square>
+            <Tabs
+                className="navigation"
+                variant="fullWidth"
+                indicatorColor="primary"
+                textColor="primary"
+                aria-label="HOME PROJECTS BULLETJOURNAL"
+            >
+                <Link to="/" className="nav-link">
+                    <Tab icon={<Logo style={iconStyles} />} aria-label="HOME" />
+                </Link>
+                <Link to="/projects" className="nav-link">
+                    <Tab
+                        icon={<ProjectsIcon style={iconStyles} />}
+                        aria-label="PROJECTS"
+                    />
+                </Link>
+                <Link to="/bulletJournal" className="nav-link">
+                    <Tab
+                        icon={<BulletJournalIcon style={iconStyles} />}
+                        aria-label="BULLETJOURNAL"
+                    />
+                </Link>
+            </Tabs>
+        </Paper>
     );
 };
 
