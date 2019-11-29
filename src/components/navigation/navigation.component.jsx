0f54fc8f8af9cf93cd09e38af50cddc1ad608eea
Paweł Benkowski <pawlo97.pb@gmail.com>
2019-11-29 22:23:24 +0100

    This code is absolute garbage, leaving it to remember the idea, will fix that later

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Paper, Tabs, Tab } from "@material-ui/core";
+import {
+    Paper,
+    Tabs,
+    Tab,
+    BottomNavigation,
+    BottomNavigationAction,
+} from "@material-ui/core";
 //Styles
 import "./navigation.styles.scss";
 // SVG's
@@ -8,57 +14,72 @@ import { ReactComponent as ProjectsIcon } from "../../assets/icons/projects.svg"
 import { ReactComponent as BulletJournalIcon } from "../../assets/icons/journal.svg";
 import { ReactComponent as Logo } from "../../assets/icons/sloth.svg";
 
-const Navigation = () => {
-    // return (
-    //     <nav className="navigation">
-    //
-    //             <div className="nav-item">
-    //                 <Logo className="icon" />
-    //             </div>
-    //         </Link>
-    //
-    //             <div className="nav-item">
-    //                 <ProjectsIcon className="icon" />
-    //                 Projects
-    //             </div>
-    //         </Link>
-    //
-    //             <div className="nav-item">
-    //                 <BulletJournalIcon className="icon" />
-    //                 Bullet Journal
-    //             </div>
-    //         </Link>
-    //     </nav>
-    // );
-    const iconStyles = { width: "6vh", height: "6vh" };
-    return (
-        <Paper square>
-            <Tabs
-                className="navigation"
-                variant="fullWidth"
-                indicatorColor="primary"
-                textColor="primary"
-                aria-label="HOME PROJECTS BULLETJOURNAL"
-            >
-                <Link to="/" className="nav-link">
-                    <Tab icon={<Logo style={iconStyles} />} aria-label="HOME" />
-                </Link>
-                <Link to="/projects" className="nav-link">
-                    <Tab
-                        icon={<ProjectsIcon style={iconStyles} />}
-                        aria-label="PROJECTS"
-                    />
-                </Link>
-                <Link to="/bulletJournal" className="nav-link">
-                    <Tab
-                        icon={<BulletJournalIcon style={iconStyles} />}
-                        aria-label="BULLETJOURNAL"
-                    />
-                </Link>
-            </Tabs>
-        </Paper>
-    );
-};
+class Navigation extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            width: window.innerWidth,
+        };
+    }
+    componentWillMount() {
+        window.addEventListener("resize", this.handleWindowSizeChange);
+    }
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.handleWindowSizeChange);
+    }
+    handleWindowSizeChange = () => {
+        this.setState({ width: window.innerWidth });
+    };
+    render() {
+        const iconStyles = { width: "6vh", height: "6vh" };
+        const isMobile = this.state.width < 768;
+        return isMobile ? (
+            <Paper square>
+                <Tabs
+                    className="navigation"
+                    variant="fullWidth"
+                    indicatorColor="primary"
+                    textColor="primary"
+                    aria-label="HOME PROJECTS BULLETJOURNAL"
+                >
+                    <Link to="/" className="nav-link">
+                        <Tab
+                            icon={<Logo style={iconStyles} />}
+                            aria-label="HOME"
+                        />
+                    </Link>
+                    <Link to="/projects" className="nav-link">
+                        <Tab
+                            icon={<ProjectsIcon style={iconStyles} />}
+                            aria-label="PROJECTS"
+                        />
+                    </Link>
+                    <Link to="/bulletJournal" className="nav-link">
+                        <Tab
+                            icon={<BulletJournalIcon style={iconStyles} />}
+                            aria-label="BULLETJOURNAL"
+                        />
+                    </Link>
+                </Tabs>
+            </Paper>
+        ) : (
+            <BottomNavigation showLabels>
+                <BottomNavigationAction
+                    label="Home"
+                    icon={<Logo style={iconStyles} />}
+                />
+                <BottomNavigationAction
+                    label="Projects"
+                    icon={<ProjectsIcon style={iconStyles} />}
+                />{" "}
+                <BottomNavigationAction
+                    label="BulletJournal"
+                    icon={<BulletJournalIcon style={iconStyles} />}
+                />
+            </BottomNavigation>
+        );
+    }
+}
 
 export default Navigation;
 // <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
