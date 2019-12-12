ad49bafef953076ab5f1d3616a301454dce81270
Paweł Benkowski <pawlo97.pb@gmail.com>
2019-12-12 10:39:32 +0100

    rough version of energies complete

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -44,6 +44,29 @@ const HomePage = () => {
                     www.flaticon.com
                 </a>
             </div>
+            <div>
+                Icons made by{" "}
+                <a href="https://www.flaticon.com/authors/srip" title="srip">
+                    srip
+                </a>{" "}
+                from{" "}
+                <a href="https://www.flaticon.com/" title="Flaticon">
+                    www.flaticon.com
+                </a>
+            </div>
+            <div>
+                Icons made by{" "}
+                <a
+                    href="https://www.flaticon.com/authors/wanicon"
+                    title="wanicon"
+                >
+                    wanicon
+                </a>{" "}
+                from{" "}
+                <a href="https://www.flaticon.com/" title="Flaticon">
+                    www.flaticon.com
+                </a>
+            </div>
         </div>
     );
 };
