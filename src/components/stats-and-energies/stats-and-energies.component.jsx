deb46e9dfaa7eb49f7de56a878f6a92e4015fe4e
Paweł Benkowski <pawlo97.pb@gmail.com>
2019-12-13 20:33:52 +0100

    It looks like shit, it doesn't work

--- src/components/stats-and-energies/stats-and-energies.component.jsx
+++ src/components/stats-and-energies/stats-and-energies.component.jsx
@@ -13,59 +13,76 @@ import { ReactComponent as Intelligence } from "../../assets/icons/stats/intelli
 import { ReactComponent as Fluency } from "../../assets/icons/stats/fluency.svg";
 // Actions
 import { setBodyEnergy } from "../../redux/stats-and-energies/stats-and-energies.actions.js";
+// Components
+import ProgressBar from "../progress-bar/progress-bar.component.jsx";
 
-const StatsAndEnergies = ({ which, body, setBodyEnergy }) => {
+const StatsAndEnergies = ({
+    which,
+    body,
+    strength,
+    emotions,
+    creativity,
+    mind,
+    intelligence,
+    soul,
+    fluency,
+}) => {
     return (
         <div className={which}>
-            <div className="container" onClick={() => setBodyEnergy(body - 1)}>
-                <div className="progress">
-                    {which === "energies" ? (
-                        <Body className="icon" />
-                    ) : (
-                        <Strength className="icon" />
-                    )}
-                    <div
-                        className="bar body"
-                        style={{ width: `${body}` }}
-                    ></div>
-                </div>
-            </div>
-            <div className="container">
-                <div className="progress">
-                    {which === "energies" ? (
-                        <Emotions className="icon" />
-                    ) : (
-                        <Creativity className="icon" />
-                    )}
-                    <div className="bar emotions"></div>
-                </div>
-            </div>
-            <div className="container">
-                <div className="progress">
-                    {which === "energies" ? (
-                        <Mind className="icon" />
-                    ) : (
-                        <Intelligence className="icon" />
-                    )}
-                    <div className="bar mind"></div>
-                </div>
-            </div>
-            <div className="container">
-                <div className="progress">
-                    {which === "energies" ? (
-                        <Soul className="icon" />
-                    ) : (
-                        <Fluency className="icon" />
-                    )}
-                    <div className="bar soul"></div>
-                </div>
-            </div>
+            <ProgressBar
+                barType="body"
+                stat={which === "energies" ? body : strength}
+                onClick={() => setBodyEnergy(50)}
+            >
+                {which === "energies" ? (
+                    <Body className="icon" />
+                ) : (
+                    <Strength className="icon" />
+                )}
+            </ProgressBar>
+            <ProgressBar
+                barType="emotions"
+                stat={which === "energies" ? emotions : creativity}
+            >
+                {which === "energies" ? (
+                    <Emotions className="icon" />
+                ) : (
+                    <Creativity className="icon" />
+                )}
+            </ProgressBar>
+            <ProgressBar
+                barType="mind"
+                stat={which === "energies" ? mind : intelligence}
+            >
+                {which === "energies" ? (
+                    <Mind className="icon" />
+                ) : (
+                    <Intelligence className="icon" />
+                )}
+            </ProgressBar>
+            <ProgressBar
+                barType="soul"
+                stat={which === "energies" ? soul : fluency}
+            >
+                {which === "energies" ? (
+                    <Soul className="icon" />
+                ) : (
+                    <Fluency className="icon" />
+                )}
+            </ProgressBar>
         </div>
     );
 };
 
 const mapStateToProps = state => ({
     body: state.statsAndEnergies.body,
+    strength: state.statsAndEnergies.strength,
+    emotions: state.statsAndEnergies.emotions,
+    creativity: state.statsAndEnergies.creativity,
+    mind: state.statsAndEnergies.mind,
+    intelligence: state.statsAndEnergies.intelligence,
+    soul: state.statsAndEnergies.soul,
+    fluency: state.statsAndEnergies.fluency,
 });
 
 const mapDispatchToProps = dispatch => ({
