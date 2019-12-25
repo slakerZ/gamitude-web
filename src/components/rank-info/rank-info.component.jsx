8ac7481138efa6de6362af342a49cbccd7ce9381
Paweł Benkowski <pawlo97.pb@gmail.com>
2019-12-25 13:05:56 +0100

    rank-info structure done

---
+++ src/components/rank-info/rank-info.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./rank-info.styles.scss";
+
+const RankInfo = () => (
+    <div className="rank-info">
+        <div className="rank-img" />
+        <h1 className="tier">F Tier</h1>
+        <h1 className="rank-name">Sloth </h1>
+    </div>
+);
+
+export default RankInfo;
--- src/pages/projects/projects.page.jsx
+++ src/pages/projects/projects.page.jsx
@@ -2,13 +2,14 @@ import React from "react";
 // Components
 import Stats from "../../components/stats/stats.component.jsx";
 import Energies from "../../components/energies/energies.component.jsx";
+import Rank from "../../components/rank/rank.component.jsx";
 // Styles
 import "./projects.styles.scss";
 
 const ProjectsPage = () => {
     return (
         <div className="projects-page">
-            <div className="rank">rank</div>
+            <Rank />
             <div className="projects">projects</div>
             <Stats />
             <Energies />
