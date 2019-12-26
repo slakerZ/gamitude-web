8ae0b95e72dd8790c30115d4af999da02dab9e9b
Paweł Benkowski <pawlo97.pb@gmail.com>
2019-12-26 14:55:58 +0100

    images load dynamically accordingly to stats

--- src/components/rank/rank.component.jsx
+++ src/components/rank/rank.component.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import "./rank.styles.scss";
+import { connect } from "react-redux";
 
 import RankInfo from "../rank-info/rank-info.component.jsx";
 import HelpIcon from "../help-icon/help-icon.component.jsx";
 
-const Rank = () => {
-    const gotRank = {
-        name: "Sloth",
-        tier: "F",
-        imageUrl: require("../../assets/ranks_default/sloth-1.jpg"),
-    };
+const Rank = ({ strength, creativity, intelligence, fluency, ranks }) => {
+    const gotRank =
+        ranks[`${strength}-${creativity}-${intelligence}-${fluency}`];
     return (
         <div className="rank">
             <RankInfo
@@ -22,4 +20,12 @@ const Rank = () => {
     );
 };
 
-export default Rank;
+const mapStateToProps = state => ({
+    strength: state.stats.strength,
+    creativity: state.stats.creativity,
+    intelligence: state.stats.intelligence,
+    fluency: state.stats.fluency,
+    ranks: state.rank.ranks,
+});
+
+export default connect(mapStateToProps)(Rank);
--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import energiesReducer from "./energies/energies.reducer";
 import statsReducer from "./stats/stats.reducer";
 import navigationReducer from "./navigation/navigation.reducer";
+import rankReducer from "./rank/rank.reducer";
 
 export default combineReducers({
     energies: energiesReducer,
     stats: statsReducer,
     navigation: navigationReducer,
+    rank: rankReducer,
 });
