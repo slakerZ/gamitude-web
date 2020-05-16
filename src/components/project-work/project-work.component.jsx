f1f45539daf0482ada0fdd60a7a3f5ce0d49cc3d
Paweł Benkowski <36889080+BennyKayer@users.noreply.github.com>
2020-05-16 20:06:11 +0200

    Feature/is110/just 5 flow state custom time (#21)
    * Tooltip for every icon that there is
    
    * Ranks integrated
    
    * No more ports
    
    * Clamped bar width, stat will be red if gone overboard
    
    * Bugfix
    
    * Just 5 completed
    
    * No more rank displays
    
    * no http before url
    
    * Custom time implemented
    
    * Flow state implemented

--- src/components/project-work/project-work.component.jsx
+++ src/components/project-work/project-work.component.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { connect } from "react-redux";
+// Selectors
+import { selectProjects } from "../../redux/projects/projects.selectors";
 // UI core
 import { makeStyles } from "@material-ui/core";
+import Input from "@material-ui/core/Input";
 // Components
 import ProjectTimer from "../project-timer/project-timer.component.jsx";
+import ProjectStopWatch from "../project-stopwatch/project-stopwatch.component.jsx";
 import ProjectToggle from "../project-toggle/project-toggle.component.jsx";
 
-const ProjectWork = ({ index }) => {
+const ProjectWork = ({ index, projects }) => {
+    const method = projects[index].method;
+
     const useStyles = makeStyles({
         container: {
             display: "flex",
@@ -15,12 +22,49 @@ const ProjectWork = ({ index }) => {
     });
     const classes = useStyles();
 
+    const [time, setTime] = React.useState(0);
+
+    const onChange = event => {
+        const value = event.target.value;
+        const isNum = /^\d+$/.test(value);
+
+        if (isNum) {
+            const isValidTime = value > 0 && value <= 360;
+            if (isValidTime) {
+                setTime(value);
+            }
+        } else if (value.length === 0) {
+            setTime(0);
+        }
+    };
+
     return (
-        <div className={classes.container}>
-            <ProjectTimer index={index} />
-            <ProjectToggle index={index} />
+        <div>
+            {method === "infinite" ? (
+                <div className={classes.container}>
+                    <ProjectStopWatch index={index} />
+                    <ProjectToggle index={index} time={time} />
+                </div>
+            ) : (
+                <div>
+                    <div className={classes.container}>
+                        <ProjectTimer index={index} />
+                        <ProjectToggle index={index} time={time} />
+                    </div>
+                    <br />
+                    <Input
+                        value={time === 0 ? "" : time}
+                        placeholder={"Custom Time"}
+                        onChange={onChange}
+                    />
+                </div>
+            )}
         </div>
     );
 };
 
-export default ProjectWork;
+const mapStateToProps = state => ({
+    projects: selectProjects(state),
+});
+
+export default connect(mapStateToProps)(ProjectWork);
