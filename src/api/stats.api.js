6559761e95806120feceaeb95ea8e163df094112
Paweł Benkowski <pawlo97.pb@gmail.com>
2020-05-16 16:53:22 +0200

    Me and the boys debugging in production

--- src/api/stats.api.js
+++ src/api/stats.api.js
@@ -1,7 +1,7 @@
 export const url =
     process.env.NODE_ENV === "development"
         ? "http://localhost:5030/api/stats/statistics/stats"
-        : "gamitude.rocks/api/stats/statistics/stats";
+        : "https://gamitude.rocks/api/stats/statistics/stats";
 
 export const headers = token => ({
     headers: {
